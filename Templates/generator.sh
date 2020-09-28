03a51f16cbbcf0d3fb37753a9bc12cffb24ed009
Manuel Tobar <manuel.tobar@mail.udp.cl>
2020-09-28 19:26:28 -0300

    templates: i'm dumb

--- Templates/generator.sh
+++ Templates/generator.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+mkdir input output
 for((i = 0; i < 10; i++)) do
     ran=$RANDOM
     name=$(printf "%02d" $(($i+1)))
     echo $ran $name
-    mkdir input output
     ./gen.o $ran > input/input$name.txt
     ./main.o < input/input$name.txt > output/output$name.txt
 done
