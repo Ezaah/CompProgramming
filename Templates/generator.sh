5f3f0ef04a3a9de797c8e96dfa4fb7131cde1c86
Manuel Tobar <manuel.tobar@mail.udp.cl>
2020-09-28 17:20:13 -0300

    fix: missed folder creation in generator

--- Templates/generator.sh
+++ Templates/generator.sh
@@ -3,6 +3,7 @@
 for((i = 0; i < 10; i++)) do
     ran=$RANDOM
     echo $ran
+    mkdir input output
     ./gen.o $ran > input/input0$i.txt
     ./main.o < input/input0$i.txt > output/output0$i.txt
 done
